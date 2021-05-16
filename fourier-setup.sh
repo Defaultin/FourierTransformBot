4c861acdba539c198554b3db0eab6c4bd829a009
Kirill <gorynovich0101@gmail.com>
2021-05-16 13:15:09 +0300

    add setup

--- fourier-setup.sh
+++ fourier-setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get -y install python3 python3-pip git
+sudo apt-get -y install python3 python3-pip git ffmpeg
 git clone https://github.com/Defaultin/FourierTransformBot.git
 cd FourierTransformBot/
 sudo pip3 install -r requirements.txt
