38289a13bfcf8ae289ff294b4c7f9fc9296d879a
Kirill <gorynovich0101@gmail.com>
2021-05-16 13:05:45 +0300

    add setup

--- fourier-setup.sh
+++ fourier-setup.sh
@@ -4,4 +4,8 @@ git clone https://github.com/Defaultin/FourierTransformBot.git
 cd FourierTransformBot/
 sudo pip3 install -r requirements.txt
 sudo pip3 install opencv-python Pillow
+sudo -H pip3 uninstall pytelegrambotapi
+sudo -H pip3 uninstall telebot
+sudo -H pip3 install telebot==0.0.3
+sudo -H pip3 install pytelegrambotapi==3.6.6
 sudo python3 main.py
